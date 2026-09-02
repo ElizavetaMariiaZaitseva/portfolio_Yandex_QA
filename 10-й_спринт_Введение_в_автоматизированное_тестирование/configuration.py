51efb3bfe4dbef4da3accc775d25192d0e017af4
ElizavetaMariiaZaitseva <lizmari2488@gmail.com>
2026-09-02 23:10:12 +0300

    Add files via upload

---
+++ 10-й_спринт_Введение_в_автоматизированное_тестирование/configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d8d0f0fe-2c5e-48b1-8aa8-fa5d95eaf765.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+MAIN_KITS = "/api/v1/kits"
---
+++ 10-й_спринт_Введение_в_автоматизированное_тестирование/data.py
@@ -0,0 +1,11 @@
+headers_user = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Елизавета",
+    "phone": "+79266635611",
+    "address": "г. Москва, ул. Юбилейная, д. 8"
+}
+
+AUTH_TOKEN = ""
